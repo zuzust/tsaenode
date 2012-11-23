32f4b8bd601981f2811ebb9db54ab94ed20af2b2
Carles Muiños <carles.ml.dev@gmail.com>
2012-11-23 19:10:12 +0100

    first commit

---
+++ src/main/java/org/coderebels/tsaenode/App.java
@@ -0,0 +1,13 @@
+package org.coderebels.tsaenode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
